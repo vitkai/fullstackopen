59841edfc3c32abee304c44b845c979d74563d6d
vitkai <39426330+vitkai@users.noreply.github.com>
2023-03-26 16:44:47 +0300

    tefactored setClicks using '...clicks'

--- part1/src/App.js
+++ part1/src/App.js
@@ -20,21 +20,11 @@ function App(props) {
     left: 0, right: 0
   })
 
-  const handleLeftClick = () => {
-    const newClicks = { 
-      left: clicks.left + 1, 
-      right: clicks.right 
-    }
-    setClicks(newClicks)
-  }
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: clicks.left + 1 })
 
-  const handleRightClick = () => {
-    const newClicks = { 
-      left: clicks.left, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
-  }
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 })
 
   // setTimeout(
   //   () => setCounter(counter + 1),
