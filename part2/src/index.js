ae55ff31091e7f6f35feadd10eada944d8ffe43d
vitkai <39426330+vitkai@users.noreply.github.com>
2023-04-21 12:02:00 +0300

    axios promises added

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 
 import App from './App'
+
+const promise = axios.get('http://localhost:3001/notes')
+console.log(promise)
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
 
 const notes = [
   {
