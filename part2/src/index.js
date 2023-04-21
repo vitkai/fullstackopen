d8b1a8db084629ba504c3bf3fd01659a679fb0f5
vitkai <39426330+vitkai@users.noreply.github.com>
2023-04-21 12:11:05 +0300

    axios moved to App

--- part2/src/index.js
+++ part2/src/index.js
@@ -7,7 +7,4 @@ import App from './App'
 const promise2 = axios.get('http://localhost:3001/foobar')
 console.log(promise2)
 
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
-})
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
