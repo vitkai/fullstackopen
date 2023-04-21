fe9bf4a18260caa827b0bc9cea5b241c74fab6af
vitkai <39426330+vitkai@users.noreply.github.com>
2023-04-21 12:07:01 +0300

    notes are being fetched form json-server

--- part2/src/index.js
+++ part2/src/index.js
@@ -4,30 +4,10 @@ import axios from 'axios'
 
 import App from './App'
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-
 const promise2 = axios.get('http://localhost:3001/foobar')
 console.log(promise2)
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
