4729c8b4e3877ed621edc375578b247697f4b7c2
vitkai <39426330+vitkai@users.noreply.github.com>
2023-04-22 11:38:08 +0300

    service for notes added

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -14,17 +15,17 @@ const App = () => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
   
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(n => n.id !== id ? n : response.data))
-    })
+    noteService
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      })
   }
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
+    noteService
+      .getAll()
       .then(response => {
-        console.log('promise fulfilled')
         setNotes(response.data)
       })
   }, [])
@@ -38,10 +39,9 @@ const App = () => {
       important: Math.random() < 0.5,
     }
   
-    axios
-      .post('http://localhost:3001/notes', noteObject)
+    noteService
+      .create(noteObject)
       .then(response => {
-        console.log(response)
         setNotes(notes.concat(response.data))
         setNewNote('')
       })
