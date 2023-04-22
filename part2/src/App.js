94e23dd6303e40438a559f1dc435c119d9c7110e
vitkai <39426330+vitkai@users.noreply.github.com>
2023-04-22 11:28:56 +0300

    toggleImportanceOf upd

--- part2/src/App.js
+++ part2/src/App.js
@@ -9,8 +9,14 @@ const App = () => {
     'a new note...'
   ) 
 
-  const toggleImportanceOf = (id) => {
-    console.log('importance of ' + id + ' needs to be toggled')
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(n => n.id !== id ? n : response.data))
+    })
   }
 
   useEffect(() => {
