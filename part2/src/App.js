8f3a594cc1da9d4bf03ae786cdea4a9d282c1b58
vitkai <39426330+vitkai@users.noreply.github.com>
2023-04-23 10:39:30 +0300

    error Notification added

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
 
 const App = () => {
@@ -8,7 +9,8 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
   const [newNote, setNewNote] = useState(
     'a new note...'
-  ) 
+  )
+  const [errorMessage, setErrorMessage] = useState('')
 
   const toggleImportanceOf = id => {
     const url = `http://localhost:3001/notes/${id}`
@@ -22,10 +24,13 @@ const App = () => {
       })
 
     .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
-        )
-        setNotes(notes.filter(n => n.id !== id))
+      setErrorMessage(
+        `Note '${note.content}' was already removed from server`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+      setNotes(notes.filter(n => n.id !== id))
       })
   }
 
@@ -65,6 +70,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
