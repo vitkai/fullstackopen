afc3d2318aeaeaf808ae881e256df5bd09f9a2c9
vitkai <39426330+vitkai@users.noreply.github.com>
2023-05-06 10:05:55 +0300

    refactored routes

--- part3/index.js
+++ part3/index.js
@@ -8,58 +8,8 @@ app.use(express.static('build'))
 const Note = require('./models/note')
 
 const cors = require('cors')
-
 app.use(cors())
 
-const mongoose = require('mongoose')
-
-if (process.argv.length<4) {
-  console.log('give username and password as argument')
-  process.exit(1)
-}
-
-const username = process.argv[2]
-const password = process.argv[3]
-
-const url =
-  `mongodb+srv://${username}:${password}@cluster0.f6w894j.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-  
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -71,14 +21,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -98,21 +43,18 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
