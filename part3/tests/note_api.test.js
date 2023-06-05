4f22a692a676c23574ab68ccfc4fefc6e1d52a1b
vitkai <39426330+vitkai@users.noreply.github.com>
2023-06-05 11:38:55 +0300

    test for a new note add verification

--- part3/tests/note_api.test.js
+++ part3/tests/note_api.test.js
@@ -48,6 +48,28 @@ test('notes are returned as json', async () => {
     )
   })
 
+  test('a valid note can be added', async () => {
+    const newNote = {
+      content: 'async/await simplifies making async calls',
+      important: true,
+    }
+  
+    await api
+      .post('/api/notes')
+      .send(newNote)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const response = await api.get('/api/notes')
+  
+    const contents = response.body.map(r => r.content)
+  
+    expect(response.body).toHaveLength(initialNotes.length + 1)
+    expect(contents).toContain(
+      'async/await simplifies making async calls'
+    )
+  })
+
 afterAll(async () => {
     await mongoose.connection.close()
   })
