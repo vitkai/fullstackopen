1cb81c76c84119a3fa4873778b8cb72a42153c6a
vitkai <39426330+vitkai@users.noreply.github.com>
2023-06-21 15:00:57 +0300

    note_app.cy.js upd

--- part2/cypress/e2e/note_app.cy.js
+++ part2/cypress/e2e/note_app.cy.js
@@ -17,7 +17,22 @@ describe('Note app', function() {
     cy.get('#username').type('root')
     cy.get('#password').type('salainen')
     cy.get('#login-button').click()
-
     cy.contains('Superuser logged in')
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('log in').click()
+      cy.get('input:first').type('root')
+      cy.get('input:last').type('salainen')
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
   })
 })
