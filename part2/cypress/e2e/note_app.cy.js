3834914801f0ea7bb3578108f623424741e66117
vitkai <39426330+vitkai@users.noreply.github.com>
2023-06-21 14:48:52 +0300

    login form tests upd

--- part2/cypress/e2e/note_app.cy.js
+++ part2/cypress/e2e/note_app.cy.js
@@ -11,4 +11,13 @@ describe('Note app', function() {
   it('login form can be opened', function() {
       cy.contains('log in').click()
   })
+
+  it('user can log in', function() {
+    cy.contains('log in').click()
+    cy.get('#username').type('root')
+    cy.get('#password').type('salainen')
+    cy.get('#login-button').click()
+
+    cy.contains('Superuser logged in')
+  })
 })
