0a365d57ce32830b119d0277b7d7e0670567070f
vitkai <39426330+vitkai@users.noreply.github.com>
2023-06-21 17:27:46 +0300

    urls configured inside cypress.config.js

--- part2/cypress/e2e/note_app.cy.js
+++ part2/cypress/e2e/note_app.cy.js
@@ -1,12 +1,12 @@
 describe('Note app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     const user = {
       name: 'Matti Luukkainen',
       username: 'mluukkai',
       password: 'salainen'
     }
-    cy.request('POST', 'http://localhost:3001/api/users/', user) 
+    cy.request('POST', `${Cypress.env('BACKEND')}/users/`, user) 
     cy.visit('http://localhost:3000')
   })
 
--- part2/cypress/support/commands.js
+++ part2/cypress/support/commands.js
@@ -24,17 +24,17 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 Cypress.Commands.add('login', ({ username, password }) => {
-    cy.request('POST', 'http://localhost:3001/api/login', {
+    cy.request('POST', `${Cypress.env('BACKEND')}/login`, {
       username, password
     }).then(({ body }) => {
       localStorage.setItem('loggedNoteappUser', JSON.stringify(body))
-      cy.visit('http://localhost:3000')
+      cy.visit('')
     })
   })
 
 Cypress.Commands.add('createNote', ({ content, important }) => {
     cy.request({
-      url: 'http://localhost:3001/api/notes',
+      url: `${Cypress.env('BACKEND')}/notes`,
       method: 'POST',
       body: { content, important },
       headers: {
@@ -42,5 +42,5 @@ Cypress.Commands.add('createNote', ({ content, important }) => {
       }
     })
   
-    cy.visit('http://localhost:3000')
+    cy.visit('')
   })
