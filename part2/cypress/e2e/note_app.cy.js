f4ede9ec6fc009e98b1a55733f84739e877c97ad
vitkai <39426330+vitkai@users.noreply.github.com>
2023-06-21 17:12:30 +0300

    login moved to custom command in test

--- part2/cypress/e2e/note_app.cy.js
+++ part2/cypress/e2e/note_app.cy.js
@@ -51,12 +51,7 @@ describe('Note app', function() {
     //   cy.get('#login-button').click()
     // })
     beforeEach(function() {
-      cy.request('POST', 'http://localhost:3001/api/login', {
-        username: 'mluukkai', password: 'salainen'
-      }).then(response => {
-        localStorage.setItem('loggedNoteappUser', JSON.stringify(response.body))
-        cy.visit('http://localhost:3000')
-      })
+      cy.login({ username: 'mluukkai', password: 'salainen' })
     })
 
     it('a new note can be created', function() {
