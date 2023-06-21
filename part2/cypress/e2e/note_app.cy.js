2b5c876d430856bd3f9c202212d6725214e45702
vitkai <39426330+vitkai@users.noreply.github.com>
2023-06-21 17:10:16 +0300

    login bypassing UI in test

--- part2/cypress/e2e/note_app.cy.js
+++ part2/cypress/e2e/note_app.cy.js
@@ -44,11 +44,19 @@ describe('Note app', function() {
   })
 
   describe('when logged in', function() {
+    // beforeEach(function() {
+    //   cy.contains('log in').click()
+    //   cy.get('#username').type('mluukkai')
+    //   cy.get('#password').type('salainen')
+    //   cy.get('#login-button').click()
+    // })
     beforeEach(function() {
-      cy.contains('log in').click()
-      cy.get('#username').type('mluukkai')
-      cy.get('#password').type('salainen')
-      cy.get('#login-button').click()
+      cy.request('POST', 'http://localhost:3001/api/login', {
+        username: 'mluukkai', password: 'salainen'
+      }).then(response => {
+        localStorage.setItem('loggedNoteappUser', JSON.stringify(response.body))
+        cy.visit('http://localhost:3000')
+      })
     })
 
     it('a new note can be created', function() {
