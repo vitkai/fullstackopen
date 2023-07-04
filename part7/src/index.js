b2dabd69ef7527793bd983f25e3f0bab1e0eadfc
vitkai <39426330+vitkai@users.noreply.github.com>
2023-07-04 17:15:43 +0300

    bootstrap login form

--- part7/src/index.js
+++ part7/src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
-import { Table } from 'react-bootstrap'
+import { Button, Form, Table } from 'react-bootstrap'
 
 import {
   BrowserRouter as Router,
@@ -77,15 +77,22 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username: <input />
-        </div>
-        <div>
-          password: <input type='password' />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <Form.Control
+            type="text"
+            name="username"
+          />
+          <Form.Label>password:</Form.Label>
+          <Form.Control
+            type="password"
+          />
+          <Button variant="primary" type="submit">
+            login
+          </Button>
+        </Form.Group>
+      </Form>
     </div>
   )
 }
