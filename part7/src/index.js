a499dba682a94d9ffaeb6cc206fbc3c8f3f001b7
vitkai <39426330+vitkai@users.noreply.github.com>
2023-07-04 17:13:38 +0300

    bootstrap table

--- part7/src/index.js
+++ part7/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
+import { Table } from 'react-bootstrap'
 
 import {
   BrowserRouter as Router,
@@ -34,13 +35,22 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
+    <Table striped>
+      <tbody>
+        {notes.map(note =>
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>
+                {note.content}
+              </Link>
+            </td>
+            <td>
+              {note.user}
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
   </div>
 )
 
