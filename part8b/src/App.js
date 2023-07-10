23692bc7efaa94d64cf30f54e5d0d6fc1223a609
vitkai <39426330+vitkai@users.noreply.github.com>
2023-07-10 16:44:32 +0300

    Persons component added

--- part8b/src/App.js
+++ part8b/src/App.js
@@ -9,18 +9,28 @@ query {
   }
 }
 `
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      <h2>Persons</h2>
+      {persons.map(p =>
+        <div key={p.name}>
+          {p.name} {p.phone}
+        </div>  
+      )}
+    </div>
+  )
+}
 
 const App = () => {
   const result = useQuery(ALL_PERSONS)
 
-  if (result.loading) {
+  if (result.loading)  {
     return <div>loading...</div>
   }
 
   return (
-    <div>
-      {result.data.allPersons.map(p => p.name).join(', ')}
-    </div>
+    <Persons persons={result.data.allPersons}/>
   )
 }
 
