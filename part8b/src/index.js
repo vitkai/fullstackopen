cdb7b00618bbaaf5df9702b133dbf099069275bd
vitkai <39426330+vitkai@users.noreply.github.com>
2023-07-10 16:08:00 +0300

    apollo provider

--- part8b/src/index.js
+++ part8b/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache, gql } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
@@ -28,4 +28,7 @@ client.query({ query })
   })
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>)
