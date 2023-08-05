e4c04908fdd6d6dffc05a615c96f3256c6bed08a
vitkai <39426330+vitkai@users.noreply.github.com>
2023-08-05 22:30:05 +0300

    update with error handling

--- part9/calculator.ts
+++ part9/calculator.ts
@@ -1,15 +1,29 @@
 type Operation = 'multiply' | 'add' | 'divide';
-type Result = string | number;
 
 
-const calculator = (a: number, b: number, op: Operation): Result => {
+const calculator = (a: number, b: number, op: Operation) : number => {
+  console.log('Performing', '<',op,'>', 'on', a, 'and', b)
+  switch(op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
 
-  if (op === 'multiply') {
-    return a * b;
-  } else if (op === 'add') {
-    return a + b;
-  } else if (op === 'divide') {
-    if (b === 0) return 'this cannot be done';
-    return a / b;
+      if (b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+
+      throw new Error('Operation is not multiply, add or divide!');
   }
+}
+
+try {
+  console.log(calculator(1, 5 , 'divide'));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: '
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
 }
