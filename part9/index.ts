a3690b228100d03f038d93da9f19096d3ecfeb81
vitkai <39426330+vitkai@users.noreply.github.com>
2023-08-27 09:32:21 +0300

    minor updates

--- part9/index.ts
+++ part9/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { calculator } from './calculator';
+import { calculator, Operation } from './calculator';
 const app = express();
 app.use(express.json());
 
@@ -8,15 +8,22 @@ app.get('/ping', (_req, res) => {
 });
 
 app.post('/calculate', (req, res) => {
+  console.log(`Request body: ${req.body}`)
+  console.log(`Request params name: ${req.params}`)
+  
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { value1, value2, op } = req.body;
+  console.log(`value1:${value1}, value2:${value2}, op:${op}`)
 
   if ( !value1 || isNaN(Number(value1)) ) {
     return res.status(400).send({ error: '...'});
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  const result = calculator(value1, value2, op);
+  const result = calculator(
+    Number(value1), Number(value2), op as Operation
+  ); 
+
   res.send({ result });
 });
 
