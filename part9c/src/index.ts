ff39f14e680c57816e5d121d332e792fbd277b35
vitkai <39426330+vitkai@users.noreply.github.com>
2023-08-28 10:18:00 +0300

    modules added

--- part9c/src/index.ts
+++ part9c/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+
+import diaryRouter from './routes/diaries';
 const app = express();
 app.use(express.json());
 
@@ -9,6 +11,10 @@ app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
+
+app.use('/api/diaries', diaryRouter);
+
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
---
+++ part9c/src/services/diaryService.ts
@@ -0,0 +1,19 @@
+import diaryData from '../../data/entries.json'
+
+import { Weather, Visibility, DiaryEntry } from '../types'
+
+
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[];
+
+const getEntries = (): DiaryEntry[] => {
+  return diaries;
+}
+
+const addDiary = () => {
+  return null;
+}
+
+export default {
+  getEntries,
+  addDiary
+};
