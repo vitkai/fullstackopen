77c7192d94b028c2d39aeca75e0d2b9e5b2bc464
vitkai <39426330+vitkai@users.noreply.github.com>
2023-09-02 23:14:43 +0300

    jsx components

--- part9d/my-app/src/index.tsx
+++ part9d/my-app/src/index.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+interface WelcomeProps {
+  name: string;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Welcome = (props: WelcomeProps)  => {
+  return <h1>Hello, {props.name}</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Welcome name="Sarah" />
+)
